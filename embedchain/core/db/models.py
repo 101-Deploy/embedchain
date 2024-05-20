2b685420c04e758671a681e8d27152e22540ac7c
Owais Athar <owaisathar2882@gmail.com>
2024-05-20 16:45:58 +0530

    Add new columns to ChatHistory table

--- embedchain/core/db/models.py
+++ embedchain/core/db/models.py
@@ -29,3 +29,6 @@ class ChatHistory(Base):
     answer = Column(Text)
     meta_data = Column(Text, name="metadata")
     created_at = Column(TIMESTAMP, default=func.current_timestamp(), index=True)
+    was_helful = Column(bool)  # Added this column to the table
+    rating = Column(Integer)  # Added this column to the table
+    feedback = Column(Text)  # Added this column to the table
